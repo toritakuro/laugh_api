e4c13f09244514b6b4bb1a165180cdc4e9522900
ifmmatsumoto <matsumoto.develop@gmail.com>
2023-06-26 23:17:27 +0900

    メール送信処理追加

--- spring_prj/src/main/java/com/c4ccup/laugh/LaughApplication.java
+++ spring_prj/src/main/java/com/c4ccup/laugh/LaughApplication.java
@@ -1,21 +1,21 @@
 package com.c4ccup.laugh;
 
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.c4ccup.laugh.util.AwsSesUtil;
 import com.c4ccup.laugh.util.JwtUtil;
-
-import io.jsonwebtoken.SignatureAlgorithm;
 
 @SpringBootApplication
 @RestController
 public class LaughApplication {
+
+    @Autowired
+    private AwsSesUtil awsSesUtil;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LaughApplication.class, args);
@@ -25,7 +25,13 @@ public class LaughApplication {
     public String home() {
         return "変更しました";
     }
-    
+
+    @RequestMapping("/demoMail")
+    public String mailDemo() {
+        awsSesUtil.send("m.masayoshi.0418@gmail.com", "title", "本文、HTMLも可能");
+        return "変更しました";
+    }
+
     @Bean
     public JwtUtil jwtUtil() {
         return new JwtUtil();

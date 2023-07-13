53575815f8f5b07ffb06c6e8cf514f2b5b3e7e05
鳥澤 拓朗 <toritakuro@me.com>
2023-07-13 20:27:49 +0900

    大喜利API(リクエストクラスの作成)

--- spring_prj/src/main/java/com/c4ccup/laugh/controller/OogiriController.java
+++ spring_prj/src/main/java/com/c4ccup/laugh/controller/OogiriController.java
@@ -6,9 +6,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.c4ccup.laugh.domain.OogiriTheme;
@@ -65,7 +65,9 @@ public class OogiriController {
      * @return
      */
     @RequestMapping(path = "/detail", method = RequestMethod.GET)
-    public ResponseEntity<OogiriResponse> getOogiriAnswers(@RequestParam int themeId) {
+    public ResponseEntity<OogiriResponse> getOogiriAnswers(@RequestBody OogiriRequest request) {
+
+        int themeId = request.getThemeId();
         try {
             OogiriTheme theme = oogiriRepository.getTheme(themeId);
             List<OogiriAnswerResponse> answers = oogiriRepository.getAllAnswers(themeId);

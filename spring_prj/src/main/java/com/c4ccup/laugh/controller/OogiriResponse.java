695c98b61035fb2f7f5cb3cde5818cb83989072d
鳥澤 拓朗 <toritakuro@me.com>
2023-08-23 00:15:15 +0900

    レスポンスクラス内のchgToIntを削除

--- spring_prj/src/main/java/com/c4ccup/laugh/controller/OogiriResponse.java
+++ spring_prj/src/main/java/com/c4ccup/laugh/controller/OogiriResponse.java
@@ -253,19 +253,4 @@ public class OogiriResponse {
         return response;
     }
 
-    /**
-     * 文字列配列をint型のリストに変換
-     * 
-     * @param strs
-     * @return
-     */
-    public List<Integer> chgToInt(String[] strs) {
-        List<Integer> intList = new ArrayList<>();
-        if (strs != null) {
-            for (String str : strs) {
-                intList.add(Integer.parseInt(str));
-            }
-        }
-        return intList;
-    }
 }

2320d855ff8a3836656b1e7a89e57b47f772506a
ifmmatsumoto <matsumoto.develop@gmail.com>
2023-09-10 21:58:01 +0900

    ファイルアップロード実装

--- spring_prj/src/main/java/com/c4ccup/laugh/util/PropBean.java
+++ spring_prj/src/main/java/com/c4ccup/laugh/util/PropBean.java
@@ -11,22 +11,56 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "config")
 public class PropBean {
 
+    /** email */
     private String email;
+    /** s3の共通URL */
+    private String s3url;
+    /** s3バケット */
+    private String bucket;
+
+
 
     /**
-     * emailを取得する
+     * emailを取得します。
      * @return email
      */
     public String getEmail() {
         return email;
     }
-
     /**
-     * emailを設定する
+     * emailを設定します。
      * @param email email
      */
     public void setEmail(String email) {
         this.email = email;
+    }
+    /**
+     * s3の共通URLを取得します。
+     * @return s3の共通URL
+     */
+    public String getS3url() {
+        return s3url;
+    }
+    /**
+     * s3の共通URLを設定します。
+     * @param s3url s3の共通URL
+     */
+    public void setS3url(String s3url) {
+        this.s3url = s3url;
+    }
+    /**
+     * s3バケットを取得します。
+     * @return s3バケット
+     */
+    public String getBucket() {
+        return bucket;
+    }
+    /**
+     * s3バケットを設定します。
+     * @param bucket s3バケット
+     */
+    public void setBucket(String bucket) {
+        this.bucket = bucket;
     }
 
 }

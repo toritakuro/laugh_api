48660e19e4f4ed3fa75b1704c76a46e4202ee4ac
hiro-mtmt <90678935+hiro-mtmt@users.noreply.github.com>
2023-10-10 00:10:13 +0900

    活動種別を 1:芸人、2:作家 に変更 特殊スキルのNULLを許容

--- data/docker-entrypoint-initdb.d/1_ddl.sql
+++ data/docker-entrypoint-initdb.d/1_ddl.sql
@@ -15,13 +15,13 @@ insert into special_skill (id, special_skill_name) values (1, '動画編集'),(2
 
 -- 利用者
 insert into c4cup.`user`(user_address,user_name,user_name_kana,user_type,password,debut_dt,gender,office_id,area_id,self_introduction,profile_img_path,delete_flg,login_at,create_at,update_at) values 
-    ('a@a','test','てすと','2','$2a$10$4.5qWU/J0IOj888EKpWr9.PoP6IF89AebM7lCCzCjoXwRvhssv7.C',null,'0',null,1,null,null,'0',TIMESTAMP '2023-09-27 12:56:28.000',null,null)
-  , ('b@b','サメゾンビ','さめぞんび','2','$2a$10$VRUXCjV9CoaGli2PUTGGderirtno.5CA/QQlp3yhaDOXTp1eBRXKi',null,'0',null,1,null,null,'0',TIMESTAMP '2023-10-01 07:14:57.000',null,null)
-  , ('c@c','ミルクボーイ','みるくぼーい','2','$2a$10$3BVoLnUhCi3ut9xRw82BHe4KgJTaRbJV4.Xfk5IgrztEFscc0iWrK',null,'0',null,1,null,null,'0',TIMESTAMP '2023-10-01 07:15:32.000',null,null)
-  , ('d@d','マヂカルラブリー','まぢかるらぶりー','1','$2a$10$pYLegX5ywTgHXGnI7B.Jv.F.s6L3EKi6cDCXBnDLSWt5lRReT4bm6',null,'0',null,1,null,null,'0',null,null,null)
-  ,('a@a','佐藤満春','さとうみつはる','1','$2a$10$4.5qWU/J0IOj888EKpWr9.PoP6IF89AebM7lCCzCjoXwRvhssv7.C',null,'0',null,1,null,null,'0',TIMESTAMP '2023-09-27 12:56:28.000',null,null)
-  , ('b@b','鈴木おさむ','すずきおさむ','1','$2a$10$VRUXCjV9CoaGli2PUTGGderirtno.5CA/QQlp3yhaDOXTp1eBRXKi',null,'0',null,1,null,null,'0',TIMESTAMP '2023-10-01 07:14:57.000',null,null)
-  , ('c@c','高須光聖','タカスミツヨシ','1','$2a$10$3BVoLnUhCi3ut9xRw82BHe4KgJTaRbJV4.Xfk5IgrztEFscc0iWrK',null,'0',null,1,null,null,'0',TIMESTAMP '2023-10-01 07:15:32.000',null,null)
-  , ('d@d','オークラ','オークラ','1','$2a$10$pYLegX5ywTgHXGnI7B.Jv.F.s6L3EKi6cDCXBnDLSWt5lRReT4bm6',null,'0',null,1,null,null,'0',null,null,null); 
+    ('a@a','test','てすと','1','$2a$10$4.5qWU/J0IOj888EKpWr9.PoP6IF89AebM7lCCzCjoXwRvhssv7.C',null,'0',null,1,null,null,'0',TIMESTAMP '2023-09-27 12:56:28.000',null,null)
+  , ('b@b','サメゾンビ','さめぞんび','1','$2a$10$VRUXCjV9CoaGli2PUTGGderirtno.5CA/QQlp3yhaDOXTp1eBRXKi',null,'0',null,1,null,null,'0',TIMESTAMP '2023-10-01 07:14:57.000',null,null)
+  , ('c@c','ミルクボーイ','みるくぼーい','1','$2a$10$3BVoLnUhCi3ut9xRw82BHe4KgJTaRbJV4.Xfk5IgrztEFscc0iWrK',null,'0',null,1,null,null,'0',TIMESTAMP '2023-10-01 07:15:32.000',null,null)
+  , ('d@d','マヂカルラブリー','まぢかるらぶりー','2','$2a$10$pYLegX5ywTgHXGnI7B.Jv.F.s6L3EKi6cDCXBnDLSWt5lRReT4bm6',null,'0',null,1,null,null,'0',null,null,null)
+  ,('a@a','佐藤満春','さとうみつはる','2','$2a$10$4.5qWU/J0IOj888EKpWr9.PoP6IF89AebM7lCCzCjoXwRvhssv7.C',null,'0',null,1,null,null,'0',TIMESTAMP '2023-09-27 12:56:28.000',null,null)
+  , ('b@b','鈴木おさむ','すずきおさむ','2','$2a$10$VRUXCjV9CoaGli2PUTGGderirtno.5CA/QQlp3yhaDOXTp1eBRXKi',null,'0',null,1,null,null,'0',TIMESTAMP '2023-10-01 07:14:57.000',null,null)
+  , ('c@c','高須光聖','タカスミツヨシ','2','$2a$10$3BVoLnUhCi3ut9xRw82BHe4KgJTaRbJV4.Xfk5IgrztEFscc0iWrK',null,'0',null,1,null,null,'0',TIMESTAMP '2023-10-01 07:15:32.000',null,null)
+  , ('d@d','オークラ','オークラ','2','$2a$10$pYLegX5ywTgHXGnI7B.Jv.F.s6L3EKi6cDCXBnDLSWt5lRReT4bm6',null,'0',null,1,null,null,'0',null,null,null); 
 
 SET FOREIGN_KEY_CHECKS = 1;
